e00efe1d42844621ee7532b2dfcd5f80583c16d2
vi <reducedprocess@gmail.com>
2019-11-14 04:10:53 -0500

    finalize sound clip behavior

--- Qurotema/Assets/Nox/Sound.cs
+++ Qurotema/Assets/Nox/Sound.cs
@@ -8,6 +8,8 @@ public class Sound : MonoBehaviour {
 	private float eneryFalloff = 1f;
 	private bool atRoot = false;
 	private SoundClip[] soundClips;
+
+	public GameObject soundEmitter;
 	
    	[System.Serializable]
    	public class Sonic { public bool oneShot; public AudioClip[] audios; public string name; }
@@ -15,7 +17,18 @@ public class Sound : MonoBehaviour {
 
 	void Start() {
 		//create sound clips from editor data
+		soundClips = new SoundClip[sounds.Length];
 
+		for (int i = 0; i < soundClips.Length; i++) {
+			GameObject temp = Instantiate(soundEmitter, Camera.main.transform);
+			soundClips[i] = temp.GetComponent<SoundClip>();
+			soundClips[i].init(sounds[i].oneShot, sounds[i].audios, sounds[i].name);
+		}
+
+		//kickstart loop clips
+		for (int i = 0; i < soundClips.Length; i++) {
+			if (!soundClips[i].isOneShot) soundClips[i].kickstart();
+		}
 	}
 
 	void Update() {
@@ -24,6 +37,8 @@ public class Sound : MonoBehaviour {
 
 		//change note at random point in time
 
+
+		//play appropriate sound set for different energy
 		if (energy > 60f) highEnergy();
 		else if (energy > 10f) lowEnergy();
 		else theme();
@@ -44,19 +59,21 @@ public class Sound : MonoBehaviour {
 
 	}
 
-	public void turnOffSound(string sound, float speed) {
-
-
-	}
-
-	//find clip and toggle sound
-	public void turnOnSound(string sound, float speed) {
-
-
+	public void modSound(string sound, float target, float speed) {
+		findClip(sound).modSound(target, speed);
 	}
 
 	//play one-shot of soundclip
-	public void shootSound(string sound) {
+	public void shootSound(string sound, int index = 100) {
+		findClip(sound).shootClip(index);
+	}
 
+	private SoundClip findClip(string sound) {
+		for (int i = 0; i < soundClips.Length; i++) {
+			if (soundClips[i].name == sound) return soundClips[i];
+		}
+
+		//too lazy to do proper exceptions
+		return new SoundClip();
 	}
 }
